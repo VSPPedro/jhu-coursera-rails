8ed169a1b6a5d77a015dfdfeadace06cf78bfb63
Pedro Paiva <pedrovsp109@gmail.com>
2018-11-16 02:35:26 -0300

    Update method find_allusers to get all user ordering by updated_at with option to declare a offset and limit of the sql

--- course2/todolists/assignment/assignment.rb
+++ course2/todolists/assignment/assignment.rb
@@ -21,9 +21,7 @@ class Assignment
   # Retrieve paginated results from DB
   #
   def find_allusers(offset, limit)
-      # accept offset and limit input parameters
-      # use the User Model class to find all Users, ordered by `updated_at` ascending, with specified row offset and row limit
-      # return a collection of User instances that represent the specified rows
+      User.order(:updated_at).offset(offset).limit(limit)
   end
 
   def find_alllists(offset, limit)
