520424dbc3fa9d6c03ef07cdbba883bf9f157c7c
Pedro Paiva <pedrovsp109@gmail.com>
2018-11-17 12:25:56 -0300

    Update get_todolist_byid method

--- course2/todolists/assignment/assignment.rb
+++ course2/todolists/assignment/assignment.rb
@@ -43,13 +43,11 @@ class Assignment
   # Get rows from DB by PK
   #
   def get_user_byid(id)
-      User.find(id)
+    User.find(id)
   end
 
   def get_todolist_byid(id)
-      # accept an id input parameter
-      # use the TodoList Model class to get the TodoList associated with the `id` primary key
-      # return the TodoList instance that matches the provided id
+    TodoList.find(id)
   end
 
   #
