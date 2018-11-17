fb0a700dd28472b873e202ebf9a9fa53341a260f
Pedro Paiva <pedrovsp109@gmail.com>
2018-11-17 12:27:45 -0300

    Update update_password method

--- course2/todolists/assignment/assignment.rb
+++ course2/todolists/assignment/assignment.rb
@@ -54,9 +54,7 @@ class Assignment
   # Update rows in DB
   #
   def update_password(id, password_digest)
-      # accept an id and password_digest input parameters
-      # use the User Model class to update the `password_digest` for the User associated with the id primary key
-      # (no return is required)
+      User.update(id,password_digest: password_digest)
   end
 
   def update_listname(id, name)
