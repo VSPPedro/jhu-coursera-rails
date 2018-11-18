cb67ad716a61c11d1211219c84d01541dc9dfc3a
Pedro Paiva <pedrovsp109@gmail.com>
2018-11-18 20:46:07 -0300

    Add relationship between User and Todolist

--- course2/module2/todolists/db/schema.rb
+++ course2/module2/todolists/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181118233739) do
+ActiveRecord::Schema.define(version: 20181118234354) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20181118233739) do
     t.date     "list_due_date"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
