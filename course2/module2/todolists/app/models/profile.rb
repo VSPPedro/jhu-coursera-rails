28c11f86483d0dfde948b1b0da4aa2678bc1840f
Pedro Paiva <pedrovsp109@gmail.com>
2018-11-20 19:09:50 -0300

    Add get_all_profiles method to profile class

--- course2/module2/todolists/app/models/profile.rb
+++ course2/module2/todolists/app/models/profile.rb
@@ -13,4 +13,8 @@ class Profile < ActiveRecord::Base
   def sue
     errors.add(:first_name, "cannot accetp Sue as a value") if first_name.eql? "Sue"
   end
+
+  def self.get_all_profiles(min_birth_year, max_birth_year)
+    Profile.where('birth_year BETWEEN ? AND ?', min_birth_year, max_birth_year).order(:birth_year)
+  end
 end
