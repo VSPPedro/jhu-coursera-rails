475c1bc5c704f07ee5454e4bb7804f977dddc02a
Pedro Paiva <pedrovsp109@gmail.com>
2018-11-19 22:18:11 -0300

    Add validation in User and Profile models

--- course2/module2/todolists/app/models/profile.rb
+++ course2/module2/todolists/app/models/profile.rb
@@ -1,3 +1,16 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  validate  :name, :denomination, :sue
+
+  def name
+    errors.add(:first_name, "cannot be empty if last_name it is empty") if first_name.nil? and last_name.nil?
+  end
+
+  def denomination
+    errors.add(:gender, "cannot have a value different from male or female") unless ["male", "female"].include? gender
+  end
+
+  def sue
+    errors.add(:first_name, "cannot accetp Sue as a value") if first_name.eql? "Sue"
+  end
 end
