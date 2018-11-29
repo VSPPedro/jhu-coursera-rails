1a82796583b980cffa5ac32b7444fa8cc91a78fc
Pedro Paiva <pedrovsp109@gmail.com>
2018-11-28 21:18:49 -0300

    Add sessions controller

--- course2/module4/todolists/config/routes.rb
+++ course2/module4/todolists/config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources :todo_lists do
     resources :todo_items
   end
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get    "/login",  to: "sessions#new",     as: :login
+  delete "/logout", to: "sessions#destroy", as: :logout
 end
