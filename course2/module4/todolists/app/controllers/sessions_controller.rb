109798c745e6d1871f5a99b3273e5a93e2ea5e12
Pedro Paiva <pedrovsp109@gmail.com>
2018-11-28 22:28:55 -0300

    Update new session

--- course2/module4/todolists/app/controllers/sessions_controller.rb
+++ course2/module4/todolists/app/controllers/sessions_controller.rb
@@ -6,14 +6,27 @@ class SessionsController < ApplicationController
   # POST /sessions
   # POST /sessions.json
   def create
+    user = User.find_by_username(sessions_params[:username])
+
+    if user && user.authenticate(sessions_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      redirect_to login_path, alert: "Invalid username/password combination"
+    end
   end
 
   # DELETE /sessions/1
   # DELETE /sessions/1.json
   def destroy
     respond_to do |format|
-      format.html { redirect_to todo_lists_url, notice: 'Todo list was successfully destroyed.' }
+      format.html { redirect_to login_path, notice: 'Todo list was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
+
+  private
+    def sessions_params
+      params.require(:user).permit(:username, :password)
+    end
 end
